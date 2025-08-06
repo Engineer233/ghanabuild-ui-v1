33bfc532bd13b04e09f5d224a7c88429daf18497
Engineer233 <nii.andrews@gmail.com>
2025-08-06 02:30:12 -0500

    Add Playwright screenshot script for artifact generation

---
+++ screenshot.js
@@ -0,0 +1,6 @@
+const { chromium } = require('playwright');
+const fs = require('fs');
+
+(async () => {
+  const url = process.env.SITE_URL || 'https://www.ghanabuildai.com';
+  if (!fs.existsSync('artifacts')) fs.mkdirSync('artifacts');
